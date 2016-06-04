4598b92a94d0753a4aef70e579af1bc748369d53
Iliyan Osenski <ossensky@mail.bg>
2016-06-04 21:03:11 +0000

    Multicast messages is now simplified

---
+++ rpi-sensor-block/perl/scripts/bcastMessageTest.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl 
+
+BEGIN {
+    pop @INC;
+    push @INC,"/opt/lz/modules";
+    
+}
+
+
+use strict;
+use warnings;
+use feature 'say';
+use UDP::Multicast;
+use Data::Dumper;
+
+my $s=UDP::Multicast->new("wlan0","225.0.1.1","65432");
+print Dumper \$s;
+$s->send("HelloWorld");
+
+__DATA__
+
+
