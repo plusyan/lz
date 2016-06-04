e1f9e814d402862c8b1c01e6d6fd3a13766fe0da
Iliyan Osenski <ossensky@mail.bg>
2016-06-04 21:01:17 +0000

    More descriptive error messages

--- rpi-sensor-block/perl/services/firewall.pl
+++ rpi-sensor-block/perl/services/firewall.pl
@@ -7,17 +7,16 @@ use feature 'say';
 print  "Starting iptables firewall ... ";
 
 my $f=`which iptables`;
+die "No 'iptables' command found !" if ( $? != 0 );
+
 chomp $f;
-exit 1 if ( $? != 0 );
-
-
 system ($f,'-F','-t','nat');
 system($f,'-Z','-t','nat');
 
 system($f,'-A', 'POSTROUTING', '-t', 'nat', '-s', '10.10.10.0/24', '-j', 'MASQUERADE');
+die "Failed to initialize the firewall !!!" if ($? != 0 );
 
-
-open (W,">/proc/sys/net/ipv4/ip_forward") or 
+open (W,">/proc/sys/net/ipv4/ip_forward") or
     die "Failed to open file: /proc/sys/net/ipv4/ip_forward . The error was: $!";
 
 print W '1';
@@ -25,12 +24,7 @@ print W '1';
 close (W) or 
     die "Failed to write /proc/sys/net/ipv4/ip_forward . The error was: $!";
 
-
-
-
-exit 1 if ($? != 0 );
-
 say "done";
-__DATA__
 
-sysctl 
+
+__END__
