b7ab79c8c62aac7b49e4f1473dd15b7e296a0477
root <ossensky@mail.bg>
2016-07-07 15:41:32 +0000

    Improved pipe control

--- rpi-sensor-block/perl/services/multicast.pl
+++ rpi-sensor-block/perl/services/multicast.pl
@@ -10,9 +10,9 @@ use warnings;
 use feature 'say';
 use UDP::Multicast;
 use Data::Dumper;
+use IO::File;
 
 my $fifoPath="/data/tmp"; # Take this from somewhere ...
-
 
 opendir(FDIR,$fifoPath) or
     die "Failed to open directory: $fifoPath. The error was: $!";
@@ -23,22 +23,21 @@ foreach (readdir(FDIR)){
     next unless (-p $fifo);
     say $fifo;
     my $pid=fork();
-    if ($pid == 0){
-    
+    if ($pid == 0){    
         say "Forked to transmit the contents of $fifo ...";
-        my $s=UDP::Multicast->new("wlan0","225.0.1.1","65432") or  # Where from to take this ???
+        my $s=UDP::Multicast->new("wlan0","225.0.1.1","65432") or ###### Where from to take this ???
             die "Failed to initiate: UDP::Multicast . The error was: " . UDP::Multicast->getLastError;
-        while ( ! (open(PIPE,"$fifo"))){
+        my $fh=IO::File->new();
+        while ( ! (open($fh,"$fifo"))){
             warn "Failed to open fife: $fifo ($!). Sleeping 5 minutes and repeating ...";
             sleep 300;
         }
-        
-        while (1) {
-            my $message=<PIPE>;
-            $s->send("$message") if ($message);
+        while (<$fh>){
+           s/[\n\r]//g;
+           $s->send("$_") if ($_);
         }
-            
-        close (PIPE);    
+        say "Fatal: Failed to read from pipe: $fifo ! Unknown error !";
+        exit 1;
     }elsif($pid > 0){
         next;
     }else{
@@ -46,15 +45,3 @@ foreach (readdir(FDIR)){
     }
 }
 close (FDIR);
-
-
-
-# *. Get the addapter, multicast and port from somewhere.
-# *. Shall we use only one multicast group ?
-# *. Shall I have one more think ?
-# *.
-
-
-my $s=UDP::Multicast->new("wlan0","225.0.1.1","65432"); # Where from to take this ???
-print Dumper \$s;
-$s->send("HelloWorld");
