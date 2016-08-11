aeba6ef06037084a4ed068b66f721825e741202f
Iliyan Osenski <iosenski@vmware.com>
2016-08-11 16:53:02 +0300

    Updated

--- rpi-control-block/perl/scripts/receiveBroadcast.pl
+++ rpi-control-block/perl/scripts/receiveBroadcast.pl
@@ -3,11 +3,14 @@ use strict;
 use warnings;
 use feature 'say';
 use IO::Socket::Multicast;
+use Data::Dumper;
 my $data="";
 
 my $s = IO::Socket::Multicast->new(LocalPort=>65432);
+print Dumper \$s;
+
 while (1){
-	$s->mcast_add('225.0.1.1','wlan0');
+	$s->mcast_add('225.0.1.1','wlp3s0');
 	$s->recv($data,65432);
 	$data=~s/[\n\r]//g;
 	say $data;
