e8f08015f4b218715aca0c8bccaafce9899dfde3
root <ossensky@mail.bg>
2016-08-11 19:25:05 +0000

    Reorder

--- rpi-control-block/perl/receiveBroadcast.pl
+++ rpi-control-block/perl/receiveBroadcast.pl
@@ -4,7 +4,7 @@ use warnings;
 use feature 'say';
 use IO::Socket::Multicast;
 my $data="";
-
+#TODO: Check if we are allready running !
 my $s = IO::Socket::Multicast->new(LocalPort=>65432);
 while (1){
 	$s->mcast_add('225.0.1.1','wlan0');
