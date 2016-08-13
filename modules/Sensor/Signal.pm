47e0d6ce8d0bc5d459f3b8b5826ba6d1beaadfa2
Iliyan Osenski <iosenski@vmware.com>
2016-08-13 11:02:18 +0300

    Parse the sensor data

--- modules/Sensor/Signal.pm
+++ modules/Sensor/Signal.pm
@@ -14,9 +14,81 @@ sub new {
 
 sub decode{
     my ($self,$string)=@_;
+    my %result=();
     if ($string eq ''){
         $self->setLastError("Empty string sent ! Need decrypted string sent by the sensor !");
+        return undef;
     }
+    
+    #TODO:  Validate the CRC first !!!
+    my ($header,$rawData)=split('\|',$string);
+    
+    # Check if there is sensor failure first !
+    if ($header=~m/err-text=(.*) {0,}/){
+        $self->setLastError("sensorError=$1");
+        return undef;
+    }
+    
+    say "h=>$header";
+    say "r=>$rawData";
+    
+    #############################################
+    #                                           #
+    # Check if we have more then one '|' sign ! #
+    #                                           #
+    #############################################
+    
+    # Parse the header here !
+    my @header=split(' ',$header);
+    if ($#header == -1){
+        $self->setLastError("String: $string does not contain valid header part ! Header and data are separeted by single '|' sign !");
+        return undef;
+    }
+    $result{header}={};
+    
+    my @data=split(' ',$rawData);
+    if ($#data == -1){
+        $self->setLastError("String: $string does not contain valid data part !  Header and data are separeted by single '|' sign !");
+        return undef;
+    }
+    $result{data}={};
+    
+    
+    foreach (@header){
+        # Ignore the CRC32 because we allready checked it, and will remove it form our working solution.
+        next if m/crc\d{2}-n=/;
+        my @varVal=split('=',$_);
+        if ($#varVal == -1){
+            $self->setLastError("Empty variable=value combination detected !");
+            return undef;
+        }
+        if ($#varVal != 1){
+            $self->setLastError("Too many '=' signs ($#varVal) in string: @varVal");
+            return undef;
+        }
+        
+        $result{header}{$varVal[0]}=$varVal[1];
+    }
+    
+    foreach (@data){
+        # Ignore the CRC32 because we allready checked it, and will remove it form our working solution.
+        next if m/crc\d{2}-n=/;
+        my @varVal=split('=',$_);
+        if ($#varVal == -1){
+            $self->setLastError("Empty variable=value combination detected in data part!");
+            return undef;
+        }
+        if ($#varVal != 1){
+            $self->setLastError("Data contains too many '=' signs ($#varVal) in string: @varVal");
+            return undef;
+        }
+        
+        $result{data}{$varVal[0]}=$varVal[1];
+    }
+    
+    say "Parsed header !!!";
+    print Dumper \%result;
+    return \%result;
 }
 
 1;
--- rpi-control-block/perl/receiveBroadcast.pl
+++ rpi-control-block/perl/receiveBroadcast.pl
@@ -5,15 +5,23 @@ use feature 'say';
 use IO::Socket::Multicast;
 use Data::Dumper;
 use Sensor::Signal;
+$|=1;
 
 my $data="";
 #TODO: Check if we are allready running !
 my $s = IO::Socket::Multicast->new(LocalPort=>65432);
-print Dumper \$s;
 
+
+my $sensor=Sensor::Signal->new;
+
+print Dumper \$sensor;
 while (1){
 	$s->mcast_add('225.0.1.1','wlp3s0');
 	$s->recv($data,65432);
 	$data=~s/[\n\r]//g;
 	say $data;
+	# Decode the data here !
+	my $sensorData=$sensor->decode($data);
+	$sensorData or say $sensor->getLastError();
+	
 }
