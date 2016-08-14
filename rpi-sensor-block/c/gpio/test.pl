737e94ed48965102bd8352d04cb177c7821251fd
root <ossensky@mail.bg>
2016-08-14 12:43:20 +0000

    Correct path to the executable and correct parameters

--- rpi-sensor-block/c/gpio/test.pl
+++ rpi-sensor-block/c/gpio/test.pl
@@ -11,10 +11,11 @@ chdir  $workDir or die "Failed to change directory to: $workDir . The error was:
 while(1){
     system("make clean");
     system("make");
-    system("/opt/lz/rpi-sensor-block/c/gpio/gpio");
+    system("/opt/lz/rpi-sensor-block/c/gpio/gpio -p 0 -d -f /data/tmp/sensor_0 -t 500");
     $workDir=<>;
     if ($workDir=~m/^q|quit|exit$/i){
         say "Bye.";
         exit 1;
     }
+    
 }
