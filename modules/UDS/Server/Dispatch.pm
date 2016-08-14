758c9ad895f4cf12d7cc7d4ce6d24db86ca99391
Iliyan Osenski <iosenski@vmware.com>
2016-08-14 21:19:10 +0300

    Fork the server correctly

--- modules/UDS/Server/Dispatch.pm
+++ modules/UDS/Server/Dispatch.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use feature 'say';
 use IO::Socket::UNIX qw( SOCK_STREAM SOMAXCONN );
-
+$| = 1;
 require "/opt/lz/modules/errorHandler.pl";
 $SIG{CHLD}='IGNORE';
 
@@ -25,38 +25,50 @@ sub server {
     
     my $listen=undef;
     my $socket=undef;
-    if (-e $socketFile){
-        unless (unlink($socketFile)){
-            $self->setLastError ("Failed to unlink socket file: $socketFile . The error was: $!");
-            return undef;
-        }
-    }
 
     unless ($listen = IO::Socket::UNIX->new(Type => SOCK_STREAM,Local => $socketFile, Listen => SOMAXCONN,)){
          $self->setLastError("Cannot create server socket. The error was: $!");
          return undef;
     }
+     
+
     
-    unless  ($socket = $listen->accept()){
-        $self->setLastError("Can't accept connection: $!");
-        return undef;
-    }
     
-    while (1){    
-        chomp (my $string = <$socket>);
+    while (1){
+        unless  ($socket = $listen->accept()){
+            $self->setLastError("Can't accept connection: $!");
+            return undef;
+        }
+        autoflush $socket 1;
+        
         #if ($string){
             my $pid=fork();
             if ($pid > 0 ){
                 say "Successfully forked process: $pid to execute the registered function !";
             }elsif ($pid == 0){
-                say "Child $$: sending string: $string to the registered function !";
-                &$function($socket,$string);
+                while (1){
+                    chomp (my $string = <$socket>);
+                    say "Child $$: sending string: $string to the registered function !";
+                
+                    unless (defined &$function){
+                        say "Child $$: the registered function is gone. Terminating.";
+                        last;
+                    }else{                
+                        &$function($socket,$string);
+                    }
+                }
                 say "Child $$: Execution completed !";
                 return 0;
             }else{           
                 say "WARNING: failed to fork process in order to execute registered function! ($!)";
                 say "Executing the registered function without fork. This server will be blocked untill the function completes it's execution !";
-                &$function($socket,$string);
+                say "NOT IMPLEMENTED !!!";
+                #unless (defined &$function){
+                #    say "The registered function is gone. Terminating.";
+                #    exit 0;
+                #}else{
+                #    &$function($socket,$string);
+                #}
             }
         #}
     }
