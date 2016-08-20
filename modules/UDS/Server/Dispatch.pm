470e6096518dd1ed24a1fdd8bdd03115edbd65ff
Iliyan Osenski <iosenski@vmware.com>
2016-08-20 23:32:35 +0300

    1. Allow multiple connections. 2. Check if the connection to the client is broken and send error back.

--- modules/UDS/Server/Dispatch.pm
+++ modules/UDS/Server/Dispatch.pm
@@ -7,8 +7,8 @@ use feature 'say';
 use IO::Socket::UNIX qw( SOCK_STREAM SOMAXCONN );
 use POSIX;
 use Array::Utils qw(:all);
-use Data::Dumper;
-use Fcntl;
+use Data::Dumper;   
+use IO::Select;
 
 $| = 1;
 require "/opt/lz/modules/errorHandler.pl";
@@ -20,42 +20,56 @@ my $errMessage=undef;
 # Handle the SIGPIPE here.
 # SIGPIPE will tell us if the connection with the pear is broken !
 $SIG{PIPE} = sub {
-     $errMessage="Lost connection to server: $!";
+     $errMessage="Lost connection to client: $!";
      $connected=0;
 };
 
 sub new {
-    my %config=(
+     my $self=shift;
+     my $s=IO::Select->new();
+     
+     unless ($s){
+          $self->setLastError("Failed to initialize IO::Select");
+          return undef;
+     }
+     
+     my %config=(
+          
         init => 'true',
-    );
+        select => \$s,
+     );
+    
     return bless \%config;
 }
 
 sub send {
-    my ($self,$socket,$message)=@_;
+    my ($self,$message)=@_;
+    my $socket=$$self{socket};
     unless ($socket){
         $self->setLastError("You need to specify the socket !");
         return undef;
     }
     
-    $socket or $socket=$$self{socket};
-    unless ($socket){
-        $self->setLastError("You tried to use send method before 'server' method. Please use the server method in order to create server first !");
-        return undef;
-    }
+    my $select=$$self{select};
     
-    $message=~s/[\n\r]//;
+    say "This is select: ";
+    print Dumper \$select;
+    
+    $message=~s/[\n\r]{0,}//g;
     $message .="\n";
+    say "About to send the following message: [$message]";
     
     my $length=length($message);
-    $socket->autoflush(1);
     
-    # TODO: Change the socket state to non-blocking if the state is blocking.
-    # Change it back once the transmission is completed successfully. 
     my $rc=syswrite ($socket, $message, $length, 0);
     unless ($connected){
-        $self->setLastError("$errMessage");
-        return undef;
+          $self->setLastError("$errMessage");
+          $$select->remove($socket);
+          $socket->shutdown(2);
+          $socket->close;
+          $socket=undef;
+          $$self{socket}=undef;
+          return undef;
     }
     if ($rc > 0) {
         while ($rc < $length){
@@ -85,11 +99,32 @@ sub send {
         $self->setLastError( "syswrite() to socket error: $!");
         return undef;
     }
+    1;
     
-}    
+}
 
-
-sub server {
+sub sendToAll{
+     my ($self,$message)=@_;
+     unless ($$self{select}){
+          $self->setLastError("Nothing to send. Missing 'seleect' object.");
+          return undef;
+     }
+     
+     my $select=$$self{select};
+     my @sockets=$select->can_write;
+     if ($#sockets == -1){
+          $self->setLastError("No sockets registered with select method. Nothing to do !");
+          return undef;
+     }
+     
+     foreach my $socket (@sockets){
+          say "UNDER CONSTRICTION !";
+     }
+     $self->setLastError("Under construction !");
+     return undef;
+}
+sub server{
+     # 1. Parameters check !
     my ($self,$socketFile,$function)=@_;
     unless ($socketFile){
         $self->setLastError("No socket file sent !");
@@ -98,98 +133,82 @@ sub server {
     
     my $ref=ref $function;
     unless ( $ref eq 'CODE'){
-        $self->setLastError("Second parameter must be reference to function, not: $ref");
+        $self->setLastError("Second parameter must be reference to function, not: [$ref]");
         return undef;
     }
     
     my $listen=undef;
     my $socket=undef;
 
-    unless ($listen = IO::Socket::UNIX->new(Type => SOCK_STREAM, Local => $socketFile, Listen => SOMAXCONN)){
-         $self->setLastError("Cannot create server socket. The error was: $!");
-         return undef;
-    }
-    
-    while (1){
-        
-        unless  ($socket = $listen->accept()){
-            $self->setLastError("Can't accept connection: $!");
-            return undef;
-        }
-        
-        
-        # Prevent "suffering from the buffering" effect
-        autoflush $socket 1;
-        
-        my $pid=fork();
-        if ($pid > 0 ){
-            say "Successfully forked process: $pid to execute the registered function !";
-        }elsif ($pid == 0){
-            while (1){
-                my $string;
-                
-                # Make sure that we use blocking socket. This is to allow the server to wait for input from the client.
-                # Non blocking I/O here causes infinite loop !!!
-                $socket->blocking(1);
-                $string = <$socket>;
-                $socket->blocking(0);
-                $string=~s/[\n\r]//g;
-                
-                unless ($string){
-                    my $message="error-text=emptyMessage";
-                    my $result=""; # Call the real socketSend function here !
-                    say "1. Result after syswrite is: $result";
-                    next;
-                }
-                
-                my $message="text=youAreGoodToGo\n";
-                
-                say "Terminate the client within 10 seconds !!!";
-                    sleep 10;
-                    say "Proceedong !";
-                    
-                    
-                
-                my $result=$self->send($socket,$message,length($message),0);
-                unless ($connected){
-                    $socket->shutdown(2);
-                    close($socket);
-                    $socket=undef;
-                    last;
-                }
-                # TODO: Make sure that we check $connected value here !
-                # TODO: Make sure that we do hard check if syswrite sent all data !!!
-                
-                
-                say "2. Result after syswrite is: $result";
-                $socket->blocking(0);
-                
-                
-                chomp $string;
-                
-                unless (defined &$function){
-                    say "Child $$: the registered function is gone. Terminating.";
-                    last;
-                }else{
-                    #SELECT->can_write($socket)
-                    # TODO: Reduce the @sockets somehow !
-                    say "Child $$: sending string: $string to the registered function !";
-                    my %config=(
-                        socket => $socket
-                    );
-                    
-                    &$function(bless (\%config,'UDS::Server::Dispatch'),$string);
+     # 2. Open the Unix Domain Socket
+     unless ($listen = IO::Socket::UNIX->new(Type => SOCK_STREAM, Local => $socketFile, Listen => SOMAXCONN)){
+          $self->setLastError("Cannot create server socket. The error was: $!");
+          return undef;
+     }
+     
+     # 3. Accept connections on the socket.
+     while (1){
+          unless ($socket = $listen->accept()){
+             $self->setLastError("Can't accept connection: $!");
+             return undef;
+          }
+         
+          # Prevent "suffering from the buffering" effect
+          autoflush $socket 1;
+         
+          my $pid=fork();
+          if ($pid > 0 ){
+               say "Successfully forked process: $pid to execute the registered function !";
+          }elsif ($pid == 0){
+               #4. Execute external function to handle the request ($string) !
+               while (1){
+                    my $string=undef;
+                   
+                    # Make sure that we use blocking socket.
+                    # This is to allow the server to wait for input from the client.
                     $socket->blocking(1);
-                }
-            }
-            say "Child $$: Execution completed !";
-            return 0;
-        }else{           
-            say "FATAL: failed to fork process in order to execute the registered function! ($!)";
-        }
-    }
+                    
+                    # Wait for connection.
+                    $string = <$socket>;
+                    
+                    # The remaining of the communication is done via non-blocking socket !
+                    $socket->blocking(0);
+                    $string=~s/[\n\r]//g;
+                   
+                    #XXXXX. Integrate the Nokia communication protocol in order to register the client!
+                    
+                    #4. Register the client socket !
+                    my $select=$$self{select};
+                    
+                    $$select->add($socket);
+                    
+                    unless (defined &$function){
+                        say "Child $$: the registered function is gone. Terminating.";
+                        $self->send($socket,"text=internalServerError:functionNotRegistered\n");
+                        $$select->remove($socket);
+                        $socket->shutdown(2);
+                        $socket->close;
+                        $socket=undef;
+                        $self->setLastError("Registered function does not exist !");
+                        return undef;
+                    }
+                   
+                    say "Child $$: sending string: $string to the registered function !";
+                    
+                    $socket->blocking(0);
+                    
+                    $$self{socket}=$socket;
+                    &$function($self,$string);
+                    last unless defined $$self{socket};
+               }
+               
+               $self->setLastError("Child execution terminated.");
+               return undef;
+          }else{
+              $self->setLastError ("FATAL: failed to fork process in order to execute the registered function! ($!)");
+              return undef;
+          }
+     }
 }
 
-
 1;
-
