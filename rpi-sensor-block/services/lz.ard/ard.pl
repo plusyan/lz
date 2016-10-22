83ac6c7bab64db54a391ef6d9b98b39abad9dae9
plusyan <ossensky@mail.bg>
2016-10-22 19:21:16 +0000

    1. Optimize the code. 2. Never give up on error.

--- rpi-sensor-block/services/lz.ard/ard.pl
+++ rpi-sensor-block/services/lz.ard/ard.pl
@@ -16,7 +16,6 @@ use Config::Ard;
 use IO::File;
 use IO::Select;
 use String::CRC32;
-use Time::HiRes qw(usleep nanosleep);
 $|=1;
 my @pids=();
 
@@ -38,18 +37,34 @@ $|++;
 my $configFile="../../config/sensor-ard.conf";
 
 # Parse the config file here !!!
-say "lz ardu version 0.1 - parsing the config file: $configFile";
+say "lz ard version 0.1 - parsing the config file: $configFile";
 
 my %cfg;
 my @validConf=();
-my %duplicate; # Hash for check for duplicate fifos and id's ...
+my %duplicate; # Hash for check for duplicate config values.
+
+
+#
+# Parse the configuration file.
+#
 
 $configFile=dirname($0) .'/' . $configFile;
-
-my $c=Config::Ard->new($configFile);
-$c or die "Failed to parse config file: $configFile. The error was:\n" . $c->getLastError;
-my $cfg=$c->parse();
-die $c->getLastError unless $cfg;
+my ($c,$cfg)=undef;
+while (1){
+    $c=Config::Ard->new($configFile);
+    unless ($c){
+        say "Failed to parse config file: $configFile. The error was:\n" . $c->getLastError;
+        say "Sleeping one 10 secondes and repeating ...";
+        sleep 10; next;
+    }
+    $cfg=$c->parse();
+    unless ($cfg){
+        say $c->getLastError;
+        say "Sleeping 10 secondes and repeating";
+        sleep 10; next;
+    }
+    last;
+}
 
 %cfg=%$cfg;
 $c=undef;
@@ -59,68 +74,66 @@ say "Creating all FIFO files required:";
 
 foreach (sort keys %cfg){
     my $fifo=$cfg{$_}{pipeFile};
-    say "FIFO: $fifo";
     print "(re)creating: $fifo ...";
     if ( -e $fifo ) { 
-        unlink ($fifo) or die "Cannot delete FIFO file: $fifo . The error was: $!";
+        unless (unlink ($fifo)){
+            say "Cannot delete FIFO file: $fifo . The error was: $!";
+            say "Sleeping 10 seconds and repeating";
+            sleep 10; redo;
+        }
     }
-    system('mknod', $fifo, 'p') && die "can't mknod $fifo: $!";
+    system('mknod', $fifo, 'p');
+    if ($? != 0 ){
+        say "can't mknod $fifo: $!";
+        say "Sleeping 10 seconds and repeating.";
+        sleep 10; redo;
+    }
     say "OK";
 }
 
 $SIG{INT}=\&terminate;
 $SIG{TERM}=\&terminate;
 
-#
-# TODO: Make sure that we empty the buffer of the /dev/ttyUSBX before we proceed. 
-# I.e. check if we have multiple measurements in the buffer. If so, discard the data !
-#
-
 foreach my $ard (sort keys %cfg){
-say "Before the fork ...";
     my $pid=fork();
     if ($pid == 0 ) {
         $|=1;
         say "Starting serial port reader for: $cfg{$ard}{port}";
-        my $serial = Device::SerialPort->new($cfg{$ard}{port}, 0) || die "Can't open $cfg{$ard}{port}: $!\n";
-        $serial->baudrate($cfg{$ard}{baudRate}) || die "Cannot set speed to $cfg{$ard}{baudRate} !";
+        my $serial=undef;
+        
         my $seq=0;
         
-        my $serialConfigFile="$cfg{$ard}{pipeFile}" . "serial.config";
-        if ( -f $serialConfigFile ){
-            print  "Configuration file: $serialConfigFile exist. Deleting it ...";
-            unless (unlink($serialConfigFile)){
-                say "Failed: $!";
-                
-            }else{
-                say "Done.";
-            }
-        }
-        print "Saving configuration file: $serialConfigFile...";
-        $serial->save($serialConfigFile) or
-            die "Failed $!";
-        say "Done";
-            
-        #
-        # We only used the object $serial in order to create the configuration file.
-        #
-        
-        $serial=undef;
         my $rw=IO::File->new();
         $rw->autoflush;
-        $serial=tie (*$rw, 'Device::SerialPort', "$cfg{$ard}{port}");
-        unless ($serial){
-            #TODO: implement alert system to inform about the problem !!!
-            die "Failed to open serial port:$cfg{$ard}{port} $!";
+        print "Using 'tie' to associate  $cfg{$ard}{port} with file handler ...";
+        while (1){
+            $serial=tie (*$rw, 'Device::SerialPort', "$cfg{$ard}{port}");
+            unless ($serial){
+                #TODO: implement alert system to inform about the problem !!!
+                say "Failed to open serial port:$cfg{$ard}{port} $!";
+                say "Sleeping 10 seconds and repeating ...";
+                sleep 10;
+                next;
+            }
+            last;
         }
+        say "Done.";
+        $serial->purge_all;
         
         my $string="";
         my $pipe=IO::File->new();
         $pipe->autoflush;
+        
         print  "Opening fifo: $cfg{$ard}{pipeFile} ...";
-        unless (open ($pipe,">","$cfg{$ard}{pipeFile}")){
-            say "Failed: $!";
-            exit 1;
+        
+        while (1){
+            unless (open ($pipe,">","$cfg{$ard}{pipeFile}")){
+                say "Failed: $!";
+                say "Sleeping 10 seconds and repeating ...";
+                sleep 10;
+                next;
+            }
+            last;
         }
         say "Done";
         my $randomID=int(rand(10000)) . int(rand(10000)) . int(rand(10000));   
@@ -134,7 +147,7 @@ say "Before the fork ...";
                     if (read($rw,$char,1)){
                         $string .=$char;    
                     }
-                    last if $char eq "\n";
+                    last if ($char eq "\n");
                 }
             }else{
                 next;
@@ -148,26 +161,25 @@ say "Before the fork ...";
             # Sometimes it is possible to receive empty string. If so, ignore it and move on.
             #TODO: Count the number of empty strings. If they reach certain ammount, ring the bell ...
             #
-            
-            if ($string=~m/[\n\r]/){
-                # Check if the string is completed by the EOL symbols, be it 0d, 0a, or both ! If so, send it via the pipe.
-                $string=~s|[\n\r]{1,}||g;
+                            
+            $string=~s|[\n\r]{1,}||g;                
+            if ($string){
                 $seq++;
-                if ($string){
-                    $string="v-f=0.1/id-s=$cfg{$ard}{id}/rndid-n=$randomID/seq-n=$seq/newseq-rxn=0|" . $string; # have the version predefined
-                    $string="crc32-n=" . crc32($string)  ."/$string";
-                    print $pipe $string . "\n";
-                    $string="";
-                }
+                $string="v-f=0.1/id-s=$cfg{$ard}{id}/rndid-n=$randomID/seq-n=$seq/newseq-rxn=0|" . $string; # have the version predefined
+                $string="crc32-n=" . crc32($string)  ."/$string";
+                print $pipe $string . "\n";
+                $string="";
             }
+            
         }
-        #TODO: Try multiple times before give up !
-        close ($pipe) or die "Cannot write to pipe. $!";
+        close ($pipe) or warn "WARNING: Cannot write to pipe: $!\nPossible data loss !";
     }elsif ($pid > 0){
         push @pids,$pid;
     }else{
         say "Failed to fork child for port: $cfg{$ard}{port}\nReason:perl fork is not working !";
-        exit 1;
+        say "Sleeping 2 seconds and repeating ...";
+        sleep 2;
+        redo;
     }
 }
 sleep 100 while 1;
